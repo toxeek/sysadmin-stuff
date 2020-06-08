27a11e2201fed38ec6e3b8936119ed290f75ba15
toxeek <avernopolis@gmail.com>
2020-06-08 22:50:16 +0000

    trying who am i for root dir

--- chk_root.sh
+++ chk_root.sh
@@ -1,5 +1,5 @@
 
 # you must be root
-HOME_DIERCTORY="$(echo $(who am i) | awk '{print $1}'")
+HOME_DIERCTORY=$(echo $(who am i) | awk '{print $1}')
 [ "$UID" -ne "0" ]  && echo "[+] you need to be r00t." >&2 && exit 1
 ROOT_DIR=$HOME_DIRECTORY
