a93a7c9f0eb6356a40282781e0f4f8779b46eaf8
toxeek <avernopolis@gmail.com>
2020-06-08 23:01:53 +0000

    drunken finx root dir

--- chk_root.sh
+++ chk_root.sh
@@ -1,5 +1,9 @@
 
 # you must be root
-HOME_DIERCTORY=$(echo $(who am i) | awk '{print $1}')
-[ "$UID" -ne "0" ]  && echo "[+] you need to be r00t." >&2 && exit 1
-ROOT_DIR=$HOME_DIRECTORY
+if [ "$UID" -ne "$EUID" ]; then
+    ROOT_DIR="/home/$SUDO_USER"
+else
+    HOME_DIERCTORY="/root"
+    [ "$UID" -ne "0" ]  && echo "[+] you need to be r00t." >&2 && exit 1
+    ROOT_DIR=$HOME_DIRECTORY
+fi
