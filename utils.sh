8bbecbef1c0e43fde89ee2fae2645ab62e420b01
toxeek <avernopolis@gmail.com>
2020-06-08 22:15:30 +0000

    adding docker, tfenv

--- utils.sh
+++ utils.sh
@@ -1,16 +1,15 @@
 
-APT="$(which apt-get)"
-GREP="$(which grep)"
-AWK="$(which awk)"
-
 export CWD=$(pwd)
 export ROOT_DIR=$(dirname $CWD)
 cfg_file="${ROOT_DIR}/sysadmin.cfg"
 ansible_err_file="$ROOT_DIR/ansible/error.log"
-system_utils="(sublime-text sshfs tree tcpdump tshark python python-boto mysql-client mysql-server python-mysqldb python-mysql.connector tfenv)"
+system_utils="(sublime-text curl sshfs tree tcpdump tshark python python-boto mysql-client mysql-server python-mysqldb python-mysql.connector tfenv docker)"
 ansible_roles="(toxeek.docker)"
-
-
+#####################
+APT="$(which apt-get)"
+GREP="$(which grep)"
+AWK="$(which awk)"
+CURL="$(which curl)"
 ################
 parsecfg() {
     local task="$1"
@@ -41,6 +40,16 @@ install_sys_utils() {
         else
             ${APT} install -y ${util}
         fi 
+        if [ "$util" == "docker" ] ; then 
+            ${APT} install apt-transport-https ca-certificates curl software-properties-common
+            ${CURL} -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+            $(which add-apt-repository) "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+            ${APT} update
+            ${APT} install docker-ce
+            systemctl enable docker
+            systemctl daemo-reload docker
+            systemctl start docker
+        fi
     done 
 }
 ################
