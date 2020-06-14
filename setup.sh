6438dfb778fe3362bd583cd970683c8f7601def9
jose <avernopolis@gmail.com>
2020-06-14 22:53:29 +0000

    added needed packages for vgm,etc.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@
 typeset -a utils_array
 typeset -a pentest_utils_array
 typeset -a nmap_nse_utils_array
+typeset -a gvm_utils_array
+typeset -a services_array
 
 export REPO_NAME="sysadmin-stuff"
 export CWD=$(pwd)
@@ -14,10 +16,14 @@ export UTILS_FILE="${REPO_ROOT_DIR}/utils.sh"
 export PENTEST_UTILS_FILE="${REPO_ROOT_DIR}/pentest/utils.sh"
 export NMAP_NSE_UTILS_FILE="${REPO_ROOT_DIR}/pentest/nmap-nse/utils.sh"
 export HELPERS_FILE=${REPO_ROOT_DIR}/helpers.sh
+export SVC_MNGR_FILE="${REPO_ROOT_DIR}/services_mngr.sh"
 # base utils in the array
-export utils_array=(build-essential apt-transport-https software-properties-common unzip wget curl gnupg-agent ca-certificates tree)
+export utils_array=(build-essential git cmake pkg-config apt-transport-https software-properties-common unzip wget curl gnupg-agent ca-certificates tree)
 export pentest_utils_array=()
 export nmap_nse_utils_array=()
+# heavy pentest tool gvm requirements
+export gvm_utils_array=(gcc g++ make bison flex libksba-dev curl redis libpcap-dev cmake pkg-config libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-gcrypt-dev libldap2-dev gnutls-bin libmicrohttpd-dev libhiredis-dev zlib1g-dev libxml2-dev libradcli-dev clang-format libldap2-dev doxygen gcc-mingw-w64 xml-twig-tools libical-dev perl-base heimdal-dev libpopt-dev libsnmp-dev python3-setuptools python3-paramiko python3-lxml python3-defusedxml python3-dev gettext python3-polib xmltoman texlive-fonts-recommended texlive-latex-extra)
+export services_array=(postgresql docker etckeeper)
 
 echo "[+] utils.sh path: $UTILS_FILE"
 echo "[+] pentest utils.sh path: $PENTEST_UTILS_FILE"
@@ -40,7 +46,8 @@ CURL="$(which curl)"
 WGET="$(which wget)"
 #### pip3 ###########
 apt-add-repository universe
-${APT} update
+${APT} update 
+# ${APT} upgrade
 
 $(which find) ${REPO_DIR} -name ".*sh" -exec chmod +x '{}' \;
 [ ! -f "${UTILS_FILE}" ] && echo "[+] no utils file found." >&2 && exit 125
@@ -56,6 +63,7 @@ echo
 . ${HELPERS_FILE}
 . ${PENTEST_UTILS_FILE}
 . ${NMAP_NSE_UTILS_FILE}
+. ${SVC_MNGR_FILE}
 
 ## add user to sudo group
 ##########
