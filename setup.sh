e39c15ad43df394e4c3fe43e1617a6bb4594f1c8
jose <avernopolis@gmail.com>
2020-06-15 11:30:38 +0000

    bulk commit, gvm heavy..

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 typeset -a utils_array
 typeset -a pentest_utils_array
 typeset -a nmap_nse_utils_array
+typeset -a gvm_utils_array
 typeset -a services_array
 
 export REPO_NAME="sysadmin-stuff"
@@ -14,14 +15,14 @@ export HOME_DIR="$HOME"
 export UTILS_FILE="${REPO_ROOT_DIR}/utils.sh"
 export PENTEST_UTILS_FILE="${REPO_ROOT_DIR}/pentest/utils.sh"
 export NMAP_NSE_UTILS_FILE="${REPO_ROOT_DIR}/pentest/nmap-nse/utils.sh"
+export GVM_UTILS_FILE="${REPO_ROOT_DIR}/pentest/gvm/utils.sh"
 export HELPERS_FILE=${REPO_ROOT_DIR}/helpers.sh
 export SVC_MNGR_FILE="${REPO_ROOT_DIR}/services_mngr.sh"
 # base utils in the array
 export utils_array=(build-essential git cmake pkg-config apt-transport-https software-properties-common unzip wget curl gnupg-agent ca-certificates tree)
-export pentest_utils_array=(gcc g++ glib-2.0 glib-2.0-dev make bison flex libksba-dev curl redis-server libpcap-dev cmake libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-gcrypt-dev libldap2-dev gnutls-bin libmicrohttpd-dev libhiredis-dev zlib1g-dev libxml2-dev libradcli-dev clang-format libldap2-dev doxygen gcc-mingw-w64 xml-twig-tools libical-dev perl-base heimdal-dev libpopt-dev libsnmp-dev python3-setuptools python3-paramiko python3-lxml python3-defusedxml python3-dev gettext python3-polib xmltoman texlive-fonts-recommended texlive-latex-extra
-)
+export pentest_utils_array=()
 export nmap_nse_utils_array=()
-# heavy pentest tool gvm requirements
+export gvm_utils_array(cc g++ glib-2.0 glib-2.0-dev make bison flex libksba-dev curl redis-server libpcap-dev cmake libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-gcrypt-dev libldap2-dev gnutls-bin libmicrohttpd-dev libhiredis-dev zlib1g-dev libxml2-dev libradcli-dev clang-format libldap2-dev doxygen gcc-mingw-w64 xml-twig-tools libical-dev perl-base heimdal-dev libpopt-dev libsnmp-dev python3-setuptools python3-paramiko python3-lxml python3-defusedxml python3-dev gettext python3-polib xmltoman texlive-fonts-recommended texlive-latex-extra)
 export services_array=(postgresql docker etckeeper redis-server)
 
 echo "[+] utils.sh path: $UTILS_FILE"
@@ -32,6 +33,7 @@ echo "[+] home dir: $HOME_DIR"
 export cfg_file="${REPO_ROOT_DIR}/sysadmin.cfg"
 export pentest_cfg_file="${REPO_ROOT_DIR}/pentest/pentest.cfg"
 export nmap_nse_cfg_file="${REPO_ROOT_DIR}/pentest/nmap-nse/nse.cfg"
+export gvm_cfg_file="${REPO_ROOT_DIR}/pentest/gvm/gvm.cfg"
 echo "[+] cfg file: $cfg_file"
 echo
 # export ansible_err_file="${REPO_ROOT_DIR}/ansible/error.log"
@@ -62,6 +64,7 @@ echo
 . ${HELPERS_FILE}
 . ${PENTEST_UTILS_FILE}
 . ${NMAP_NSE_UTILS_FILE}
+. ${GVM_UTILS_FILE}
 . ${SVC_MNGR_FILE}
 
 ## add user to sudo group
@@ -75,6 +78,8 @@ install_sys_utils
 install_pentest_utils
 #########
 install_nmap_nse_utils
+#########
+install_gvm_utils
 #########
 ## add user to fuse group for sshfs
 #########
