7b53649ced05e4f434105e95b35570c232aaece6
toxeek <avernopolis@gmail.com>
2020-06-20 21:57:17 +0000

    improving outout

--- services_mngr.sh
+++ services_mngr.sh
@@ -2,9 +2,12 @@
 utils_svc_mngr() {
     echo "[+] starting serVice mngr .."
     SYSCTL=$(which systemctl)
+    echo "[+] doing a daemon-reload .."
     $(which systemctl) daemon-reload
     for svc in ${services_array[*]}; do 
         if ${SYSCTL} list-units --type service | grep -q $svc; then
+            echo 
+            echo "[+] retarting, and enabling $svc .."
             ${SYSCTL} restart $svc
             ${SYSCTL} enable $svc
         fi
