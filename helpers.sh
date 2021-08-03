ad6957f2e6f7df75ac4c33174eacb8a37caa246d
toxeek <avernopolis@gmail.com>
2021-08-03 17:11:43 +0200

    changing who

--- helpers.sh
+++ helpers.sh
@@ -1,6 +1,6 @@
 ################
 addUsertoGroup() {
-    local WHO=$(SUDO_USER)
+    local WHO=${SUDO_USER}
     local UGROUPS=$(groups $WHO | grep $1)
     [ ! -n "$UGROUPS" ] && $(which usermod) -aG "$1" $WHO
 
