585e2c30476e8e349ffc4f3ec4629cc702bc5407
toxeek <avernopolis@gmail.com>
2021-08-04 15:18:16 +0200

    reverting to apt-get

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ echo
 # export ansible_roles="(toxeek.docker)"
 #####################
 BASH=$(which bash)
-APT="$(which apt)"
+APT="$(which apt-get)"
 SNAP="$(which snap)"
 AWK="$(which awk)"
 CURL="$(which curl)"
