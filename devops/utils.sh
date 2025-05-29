5b5166c62a928b11e1d06cf4ed6a200d6cd04fe7
toxeek <avernopolis@gmail.com>
2025-05-29 19:29:28 +0200

    removing sudo

--- devops/utils.sh
+++ devops/utils.sh
@@ -52,7 +52,7 @@ install_docker() {
         ${APT} install -y ca-certificates curl
         install -m 0755 -d /etc/apt/keyrings
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        chmod a+r /etc/apt/keyrings/docker.asc
         echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
